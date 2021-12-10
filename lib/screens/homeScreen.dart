ce230aa4309b9740392e2f60648bcb160740738b
shimaa2000 <69422368+shimaa2000@users.noreply.github.com>
2021-12-10 17:47:11 +0200

    Add files via upload

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -3,9 +3,11 @@ import 'package:graduation_project/screens/details_screen.dart';
 import 'package:graduation_project/screens/edit_profile.dart';
 import 'package:graduation_project/screens/profile.dart';
 import '../dummy_data.dart';
-import '../layout/card_widget.dart'; int index=0;
-class HomeScreen extends StatelessWidget {
+import '../layout/card_widget.dart';
 
+int index = 0;
+
+class HomeScreen extends StatelessWidget {
   static const routeName = 'home_screen';
 
   // List<PostModel> posts = [
@@ -28,7 +30,8 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Padding(
               padding:
@@ -69,15 +72,10 @@ class HomeScreen extends StatelessWidget {
               padding: const EdgeInsets.only(left: 8.0),
               child: Text(
                 'Top Ads',
-                style: TextStyle(
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: Theme.of(context).textTheme.headline3,
               ),
             ),
-            SizedBox(
-              height: 10.0,
-            ),
+
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
@@ -90,7 +88,7 @@ class HomeScreen extends StatelessWidget {
                               DetailsScreen.routeName,
                               arguments: data.id,
                             );
-                            index=data.id-1;
+                            index = data.id - 1;
                           },
                           child: Column(
                             children: [
@@ -111,44 +109,82 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+
             SizedBox(
               height: 10.0,
             ),
             Text(
               'Available Items',
-              style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+              style: Theme.of(context).textTheme.headline3,
             ),
             SizedBox(
               height: 10.0,
             ),
-            // GridView(
-            //   padding: EdgeInsets.all(25),
-            //   children: DUMMY_DATA
-            //       .map(
-            //         (data) => Column(
-            //           children: [
-            //             CardWidget(
-            //               width: 175,
-            //               height: 300,
-            //               name: data.name,
-            //               date: '',
-            //               imgUrl: data.ImgUrl,
-            //               title: data.title,
-            //               price: data.price,
-            //               size: data.size,
-            //               isFav: data.isFav,
-            //             ),
-            //           ],
-            //         ),
-            //       )
-            //       .toList(),
-            //   gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            //     maxCrossAxisExtent: 200,
-            //     childAspectRatio: 3 / 2,
-            //     crossAxisSpacing: 20,
-            //     mainAxisSpacing: 20,
-            //   ),
-            // ),
+            FittedBox(
+              fit: BoxFit.contain,
+              child: Row(
+                children: [
+                 Column(children: [
+                   ...DUMMY_DATA
+                       .map(
+                         (data) => InkWell(
+                       onTap: () {
+                         Navigator.of(context).pushNamed(
+                           DetailsScreen.routeName,
+                           arguments: data.id,
+                         );
+                         index = data.id - 1;
+                       },
+                       child: Column(
+                         children: [
+                           CardWidget(
+                             name: data.name,
+                             date: data.date,
+                             imgUrl: data.ImgUrl,
+                             title: data.title,
+                             price: data.price,
+                             size: data.size,
+                             isFav: data.isFav,
+                           ),
+                         ],
+                       ),
+                     ),
+                   )
+                       .toList(),
+
+                 ],),
+                  Column(children: [
+                    ...DUMMY_DATA
+                        .map(
+                          (data) => InkWell(
+                        onTap: () {
+                          Navigator.of(context).pushNamed(
+                            DetailsScreen.routeName,
+                            arguments: data.id,
+                          );
+                          index = data.id - 1;
+                        },
+                        child: Column(
+                          children: [
+                            CardWidget(
+                              name: data.name,
+                              date: data.date,
+                              imgUrl: data.ImgUrl,
+                              title: data.title,
+                              price: data.price,
+                              size: data.size,
+                              isFav: data.isFav,
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                        .toList(),
+
+                  ],),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -161,7 +197,9 @@ class HomeScreen extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     InkWell(
-                      onTap: (){Navigator.pushNamed(context, Profile.routeName);},
+                      onTap: () {
+                        Navigator.pushNamed(context, Profile.routeName);
+                      },
                       child: CircleAvatar(
                         backgroundColor: Colors.blue,
                         radius: 30.0,
@@ -181,7 +219,11 @@ class HomeScreen extends StatelessWidget {
             ),
             ListTile(
               leading: Icon(Icons.edit),
-              title: InkWell(onTap:(){Navigator.pushNamed(context, EditProfile.routeName);},child: Text('Edit Profile')),
+              title: InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, EditProfile.routeName);
+                  },
+                  child: Text('Edit Profile')),
             ),
             Container(
               width: double.infinity,
@@ -212,67 +254,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-//         SingleChildScrollView(
-//           scrollDirection: Axis.horizontal,
-//           child: Row(
-//             children: [
-//             Container(
-//             height: 260.0,
-//             width: 260,
-//     child: Column(
-//     children: [
-//     Row(
-//     children: [
-//     CircleAvatar(
-//     radius: 20.0,
-//     ),
-//     SizedBox(width: 6.0,),
-//     Text('${AppCubit.get(context).posts[name]}'),
-//     SizedBox(width: 6.0,),
-//     Text('${post.date}'),
-//     ],
-//     ),
-//     SizedBox(
-//     height: 16.0,
-//     ),
-//     Container(
-//     color: Colors.black,
-//     ),
-//     SizedBox(
-//     height: 16.0,
-//     ),
-//     Row(
-//     children: [
-//     Text('${post.title}'),
-//     SizedBox(width: 6.0,),
-//     Text('${post.price}'),
-//     ],
-//     ),
-//     SizedBox(
-//     height: 16.0,
-//     ),
-//     Row(
-//     children: [
-//     Text('${post.size}'),
-//     Icon(Icons.favorite_outline),
-//     ],
-//     )
-//     ],
-//     ),
-//     );
-//             ],
-//           ),
-//         ),
-
-// // ListView.separated(
-// // itemBuilder: (context, index) =>
-// // AppCubit().buildTopAdItem(AppCubit().posts[index]),
-// // separatorBuilder: (context, index) => Padding(
-// // padding:
-// // const EdgeInsets.only(right: 8.0, top: 4.0, bottom: 4.0),
-// // child: Container(
-// // height: 1.0,
-// // color: Colors.grey,
-// // ),
-// // ),
-// // itemCount: 10),
