4e864f55a2312b935f552e7585a7d61ddfa15cfe
hadeer moustafa <71672005+hadeermoustafa118@users.noreply.github.com>
2021-12-12 00:09:19 +0200

    Create startApp.dart

---
+++ lib/screens/startApp.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/screens/activity_taps.dart';
+import 'package:graduation_project/screens/homeScreen.dart';
+import 'package:graduation_project/screens/messages.dart';
+import 'package:graduation_project/screens/profile.dart';
+class StartApp extends StatefulWidget {
+  static const routeName='start_app';
+  const StartApp({Key? key}) : super(key: key);
+
+  @override
+  _StartAppState createState() => _StartAppState();
+}
+
+class _StartAppState extends State<StartApp> {
+  int currentIndex= 0;
+ List <Widget> pages =[HomeScreen(), Profile(),ActivityTaps(), Messages() ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.deepPurple,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_activity),
+            label: 'Activites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: 'Messages',
+          ),
+        ],
+      ),
+    );
+  }
+}
