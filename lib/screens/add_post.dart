d658fbb0d0a3c2f025086a7dd14e81c95bb6e727
shimaa2000 <shimaa5256@gmail.com>
2021-12-11 21:41:07 +0200

    success

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -23,210 +23,214 @@ class _AddPostState extends State<AddPost> {
         MaterialPageRoute(builder: (context) => ImageFromGalleryEx(type)));
   }
 
-  final userController = TextEditingController();
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final colorController = TextEditingController();
   String dropdownValue = 'One';
   String dropdownsize = 'L';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15.0, horizontal: 8),
-                child: Text(
-                  'Enter Your Product data',
-                  style: Theme.of(context).textTheme.headline5,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15.0, horizontal: 8),
+                  child: Text(
+                    'Enter Your Product data',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: MaterialButton(
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: MaterialButton(
+                    color: Colors.deepPurple,
+                    child: Text(
+                      "Pick Image from Gallery",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      _handleURLButtonPress(context, ImageSourceType.gallery);
+                    },
+                  ),
+                ),
+                MaterialButton(
                   color: Colors.deepPurple,
                   child: Text(
-                    "Pick Image from Gallery",
+                    "Pick Image from Camera",
                     style: TextStyle(
                         color: Colors.white70, fontWeight: FontWeight.bold),
                   ),
                   onPressed: () {
-                    _handleURLButtonPress(context, ImageSourceType.gallery);
+                    _handleURLButtonPress(context, ImageSourceType.camera);
                   },
                 ),
-              ),
-              MaterialButton(
-                color: Colors.deepPurple,
-                child: Text(
-                  "Pick Image from Camera",
-                  style: TextStyle(
-                      color: Colors.white70, fontWeight: FontWeight.bold),
+
+                SizedBox(
+                  height: 20,
                 ),
-                onPressed: () {
-                  _handleURLButtonPress(context, ImageSourceType.camera);
-                },
-              ),
 
-              SizedBox(
-                height: 20,
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(3.0),
-                child: Row(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      textBaseline: TextBaseline.alphabetic,
-                      children: [
-                        Text('Title',
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 27,),
-                        Text('Description',
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 27,),
-                        Text('Color',
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 27,),
-                        Text('Size',
-                            style: Theme.of(context).textTheme.headline5),
-                        SizedBox(height: 27,),
-                        Text('Type',
-                            style: Theme.of(context).textTheme.headline5),
-                      ],
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width / 1.5,
-                          height: 50,
-                          child: BoxTextField(
-                              controller: userController,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'please confirm your Title';
-                                }
-                                return null;
-                              },
-                              hint: '',
-                              icon: Icon(Icons.title),
-                              label: '',
-                              onTap: () {}),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width / 1.5,
-                          height: 50,
-                          child: BoxTextField(
-                              controller: userController,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'please confirm your decription';
-                                }
-                                return null;
-                              },
-                              hint: '',
-                              icon: Icon(Icons.description_outlined ,color: Colors.black54,),
-                              label: '',
-                              onTap: () {}),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width / 1.5,
-                          height: 50,
-                          child: BoxTextField(
-                              controller: userController,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'please confirm your color';
-                                }
-                                return null;
-                              },
-                              hint: '',
-                              icon: Icon(Icons.color_lens_outlined),
-                              label: '',
-                              onTap: () {}),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width / 1.5,
-                          height: 50,
-                          child:Padding(
-                            padding: const EdgeInsets.symmetric(horizontal  : 28.0),
-                            child: DropdownButton<String>(
-                              value: dropdownsize,
-                              icon: const Icon(Icons.arrow_drop_down_sharp),
-                              elevation: 16,
-                              style: const TextStyle(color: Colors.black),
-                              underline: Container(
-                                height: 2,
-                                color: Colors.black26,
+                Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: Row(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text('Title',
+                              style: Theme.of(context).textTheme.headline5),
+                          SizedBox(height: 27,),
+                          Text('Description',
+                              style: Theme.of(context).textTheme.headline5),
+                          SizedBox(height: 27,),
+                          Text('Color',
+                              style: Theme.of(context).textTheme.headline5),
+                          SizedBox(height: 27,),
+                          Text('Size',
+                              style: Theme.of(context).textTheme.headline5),
+                          SizedBox(height: 27,),
+                          Text('Type',
+                              style: Theme.of(context).textTheme.headline5),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            height: 50,
+                            child: BoxTextField(
+                                controller: titleController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'please confirm your Title';
+                                  }
+                                  return null;
+                                },
+                                hint: '',
+                                icon: Icon(Icons.title),
+                                label: '',
+                                onTap: () {}),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            height: 50,
+                            child: BoxTextField(
+                                controller: descriptionController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'please confirm your description';
+                                  }
+                                  return null;
+                                },
+                                hint: '',
+                                icon: Icon(Icons.description_outlined ,color: Colors.black54,),
+                                label: '',
+                                onTap: () {}),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            height: 50,
+                            child: BoxTextField(
+                                controller: colorController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'please confirm your color';
+                                  }
+                                  return null;
+                                },
+                                hint: '',
+                                icon: Icon(Icons.color_lens_outlined),
+                                label: '',
+                                onTap: () {}),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            height: 50,
+                            child:Padding(
+                              padding: const EdgeInsets.symmetric(horizontal  : 28.0),
+                              child: DropdownButton<String>(
+                                value: dropdownsize,
+                                icon: const Icon(Icons.arrow_drop_down_sharp),
+                                elevation: 16,
+                                style: const TextStyle(color: Colors.black),
+                                underline: Container(
+                                  height: 2,
+                                  color: Colors.black26,
+                                ),
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    dropdownsize = newValue!;
+                                  });
+                                },
+                                items: <String>['S', 'L', 'XL', 'XXL']
+                                    .map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: SizedBox(width:190,child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                                      child: Text(value),
+                                    )),
+                                  );
+                                }).toList(),
                               ),
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  dropdownsize = newValue!;
-                                });
-                              },
-                              items: <String>['S', 'L', 'XL', 'XXL']
-                                  .map<DropdownMenuItem<String>>((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: SizedBox(width:190,child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                                    child: Text(value),
-                                  )),
-                                );
-                              }).toList(),
                             ),
                           ),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width / 1.5,
-                          height: 50,
-                          child:Padding(
-                            padding: const EdgeInsets.symmetric(horizontal  : 28.0),
-                            child: DropdownButton<String>(
-                              value: dropdownValue,
-                              icon: const Icon(Icons.arrow_drop_down_sharp,),
-                              elevation: 16,
-                              style: const TextStyle(color: Colors.black,),
-                              underline: Container(
-                                height: 2,
-                                width: MediaQuery.of(context).size.width / 1.5,
-                                color: Colors.black26,
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.5,
+                            height: 50,
+                            child:Padding(
+                              padding: const EdgeInsets.symmetric(horizontal  : 28.0),
+                              child: DropdownButton<String>(
+                                value: dropdownValue,
+                                icon: const Icon(Icons.arrow_drop_down_sharp,),
+                                elevation: 16,
+                                style: const TextStyle(color: Colors.black,),
+                                underline: Container(
+                                  height: 2,
+                                  width: MediaQuery.of(context).size.width / 1.5,
+                                  color: Colors.black26,
+                                ),
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    dropdownValue = newValue!;
+                                  });
+                                },
+                                items: <String>['One', 'Two', 'Free', 'Four']
+                                    .map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child:  SizedBox(width:190,child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                                      child: Text(value),
+                                    )),
+                                  );
+                                }).toList(),
                               ),
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  dropdownValue = newValue!;
-                                });
-                              },
-                              items: <String>['One', 'Two', 'Free', 'Four']
-                                  .map<DropdownMenuItem<String>>((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child:  SizedBox(width:190,child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                                    child: Text(value),
-                                  )),
-                                );
-                              }).toList(),
                             ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15.0),
-                child: DefaultButton(onPressedFun: (){}, text: 'Next'),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  child: DefaultButton(onPressedFun: (){}, text: 'Next'),
+                )
+              ],
+            ),
           ),
         ),
       ),
