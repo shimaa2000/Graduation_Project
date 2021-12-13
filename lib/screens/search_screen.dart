290a4a31e02530d08617bc7c01a5fdc20f97abc3
hadeer moustafa <71672005+hadeermoustafa118@users.noreply.github.com>
2021-12-13 23:26:23 +0200

    Update search_screen.dart

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_project/screens/Search_result_screen.dart';
+import 'package:graduation_project/shared/boxtextfield.dart';
+import 'package:graduation_project/shared/defaultButton.dart';
+import 'package:intl/intl.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
   State<SearchScreen> createState() => _SearchScreenState();
 }
-enum productFor {Sell,Rent}
-productFor? typeFor=productFor.Rent;
+
+enum productFor { Sell, Rent }
+productFor? typeFor = productFor.Rent;
+
 class _SearchScreenState extends State<SearchScreen> {
   var ScaffoldKey = GlobalKey<ScaffoldState>();
   var formKey = GlobalKey<FormState>();
-  String dropdownValue = 'Red';
-  List <String> colorList = ['Red', 'Blue', 'Gray'];
+  String dropdownValueColor = 'Red';
+  List<String> colorList = ['Red', 'Blue', 'Gray'];
+  String dropdownValueType = 'Bodycon';
+  List<String> typeList = ['Bodycon', 'A-line', 'Paesant'];
+  double price = 250;
+  var dateController = TextEditingController();
+
 
   @override
   void initState() {
@@ -31,9 +41,7 @@ class _SearchScreenState extends State<SearchScreen> {
           child: Column(
             children: [
               GestureDetector(
-                onTap: () {
-
-                },
+                onTap: () {},
                 child: Container(
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
@@ -77,112 +85,264 @@ class _SearchScreenState extends State<SearchScreen> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.deepPurple,
         onPressed: () {
-          showModalBottomSheet<void>(context: context, builder: (BuildContext ctx){
-            return Container(
-              height: 400,
-              width: MediaQuery.of(context).size.width,
-              child: Container(
-                height: 400,
-              child: Column (children: [
-                Row(
-                  children: [
-                    Icon(Icons.filter_list_rounded, size: 45,color: Colors.deepPurple,),
-                    SizedBox(width: 15,),
-                    Text('Filter',style: TextStyle( color: Colors.deepPurple, fontSize: 30, fontWeight: FontWeight.bold),)
-                  ],
-                ),
-                SizedBox(height: 10,),
-                Row(
-                  textBaseline: TextBaseline.ideographic,
-                  children: [
-                    Text('For',style: Theme.of(context).textTheme.headline3,),
-                    SizedBox(width: 20,),
-                   Row(
-                     children: [
-                       Row(
-                         children: [
-                           Text('sell'),
-                           Container(
-                             width: 20,
-                             child: ListTile(
-                               leading: Radio<productFor>(
-                                 value: productFor.Sell,
-                                 groupValue: typeFor,
-                                 onChanged: (value) { setState(() {
-                                   typeFor=value ;
-                                   print(value);
-                                 }); },
-                               ),
-                             ),
-                           ),
-                           SizedBox(width: 10,),
-                           Text('Rent',style: Theme.of(context).textTheme.headline5,),
-                         ],
-                       ),
-                       SizedBox(width: 30,),
+          showModalBottomSheet<void>(
+              context: context,
+              builder: (BuildContext ctx) {
+                return Container(
+                    height: 800,
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                        height: 800,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.filter_list_rounded,
+                                  size: 45,
+                                  color: Colors.deepPurple,
+                                ),
+                                SizedBox(
+                                  width: 15,
+                                ),
+                                Text(
+                                  'Filter',
+                                  style: TextStyle(
+                                      color: Colors.deepPurple,
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              textBaseline: TextBaseline.ideographic,
+                              children: [
+                                Text(
+                                  'For',
+                                  style: Theme.of(context).textTheme.headline3,
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Row(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Text('sell'),
+                                        Container(
+                                          width: 20,
+                                          child: ListTile(
+                                            leading: Radio<productFor>(
+                                              value: productFor.Sell,
+                                              groupValue: typeFor,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  typeFor = value;
+                                                  print(value);
+                                                });
+                                              },
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          'Rent',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline5,
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      width: 30,
+                                    ),
+                                    Row(
+                                      textBaseline: TextBaseline.ideographic,
+                                      children: [
+                                        Container(
+                                          width: 25,
+                                          child: ListTile(
+                                            leading: Radio<productFor>(
+                                              value: productFor.Rent,
+                                              groupValue: typeFor,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  typeFor = value;
+                                                  print(value);
+                                                });
+                                              },
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          'Sell',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline5,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Colour',
+                                  style: Theme.of(context).textTheme.headline3,
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Expanded(
+                                    child: DropdownButton(
+                                  value: dropdownValueColor,
+                                  icon: Icon(Icons.arrow_drop_down),
+                                  elevation: 16,
+                                  style: TextStyle(color: Colors.deepPurple),
+                                  underline: Container(
+                                    height: 2,
+                                    color: Colors.deepPurple,
+                                  ),
+                                  onChanged: (String? newValue) {
+                                    setState(() {
+                                      dropdownValueColor = newValue!;
+                                    });
+                                  },
+                                  // ignore: non_constant_identifier_names
+                                  items: colorList
+                                      .map<DropdownMenuItem<String>>(
+                                          (String? value) {
+                                    return DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(value!),
+                                    );
+                                  }).toList(),
+                                )),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Type',
+                                  style: Theme.of(context).textTheme.headline3,
+                                ),
+                                SizedBox(
+                                  width: 35,
+                                ),
+                                Expanded(
+                                  child: DropdownButton(
+                                    value: dropdownValueType,
+                                    icon: Icon(Icons.arrow_drop_down),
+                                    elevation: 16,
+                                    style: TextStyle(color: Colors.deepPurple),
+                                    underline: Container(
+                                      height: 2,
+                                      color: Colors.deepPurple,
+                                    ),
+                                    onChanged: (String? newValue2) {
+                                      setState(() {
+                                        dropdownValueType = newValue2!;
+                                      });
+                                    },
+                                    // ignore: non_constant_identifier_names
+                                    items: typeList
+                                        .map<DropdownMenuItem<String>>(
+                                            (String? value2) {
+                                      return DropdownMenuItem<String>(
+                                        value: value2,
+                                        child: Text(value2!),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Price',
+                                  style: Theme.of(context).textTheme.headline3,
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Expanded(
+                                  child: Slider(
+                                    value: price,
+                                    onChanged: (double value) {
+                                      setState(() => {price = value});
+                                    },
+                                    min: 200,
+                                    max: 1200,
+                                    divisions: 1000,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Date',
+                                  style: Theme.of(context).textTheme.headline3,
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                                Expanded(
+                                  child: BoxTextField(
+                                    label: 'Date',
+                                    padding: 10,
+                                    margin: 10,
+                                    icon: Icon(Icons.calendar_today_rounded), hint: 'Pick A Date', validator: (String? value) { if (value!.isEmpty) {
+                                    return 'date must not be empty';
+                                  } }, controller: dateController,
+                                    onTap: (){
+                                      showDatePicker(
+                                        context: context,
+                                        initialDate: DateTime.now(),
+                                        firstDate: DateTime.now(),
+                                        lastDate: DateTime(2022),
+                                      ).then((value) {
+                                        dateController.text =
+                                            DateFormat.yMMMd().format(value!);
+                                      }).catchError((error) {
+                                        print(error.toString());
+                                      });
+                                    },
+                                  ),
+                                ),
+                                
+                                    ],
+                            ),
 
-                       Row(
-                         textBaseline: TextBaseline.ideographic,
-                         children: [
-                           Container(
-                               width: 25,
-                               child:
-                               ListTile(
-                                 leading: Radio<productFor>(
-                                   value: productFor.Rent,
-                                   groupValue: typeFor,
-                                   onChanged: (value) { setState(() {
-                                     typeFor=value ;
-                                     print(value);
-                                   }); },
-                                 ),
-                               ),
-                             ),
-                           SizedBox(width: 10,),
-                           Text('Sell',style: Theme.of(context).textTheme.headline5,),
-                         ],
-                       ),
-                     ],
-                   )
-                  ],
-                ),
-                SizedBox(height: 10,),
-                Row(
-                  children: [
-                    Text('Colour', style: Theme.of(context).textTheme.headline3,),
-                    SizedBox(width: 20,),
-                    Expanded(
-                        child:DropdownButton(
-                        value: dropdownValue,
-                        icon: Icon(Icons.arrow_drop_down),
-                        elevation: 16,
-                        style: TextStyle(color: Colors.deepPurple),
-                        underline: Container(
-                          height: 2,
-
-                          color: Colors.deepPurple,
-                        ),
-                        onChanged: (String? newValue){
-                          setState(() {
-                            dropdownValue = newValue!;
-                          });
-                        },
-                        // ignore: non_constant_identifier_names
-                        items: colorList.map<DropdownMenuItem<String>>((String? value){
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value!),);
-                        }).toList(),
-                      )),
-
-
-                  ],
-                ),
-
-              ],)
-              )
-            );
-          });
+                            DefaultButton(text: 'Search', onPressedFun: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> SearchResult()));
+                            },),
+                          ],
+                        )));
+              });
         },
         child: Icon(Icons.filter_alt_outlined),
       ),
